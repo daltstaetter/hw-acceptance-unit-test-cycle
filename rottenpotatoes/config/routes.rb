c3b0066dcc1d7de6703385bf1e13fa947006b6a2
EC2 Default User <ec2-user@ip-172-31-29-159.us-east-2.compute.internal>
2018-10-03 23:52:46 +0000

    Added route to the Same Director View
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-29-159.us-east-2.compute.internal>

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -3,5 +3,7 @@ Rottenpotatoes::Application.routes.draw do
   # map '/' to be a redirect to '/movies'
   root 'movies#index'
   #root :to => redirect('/movies')
-  resources :movies
+  resources :movies do
+      get "same_director" # points to same_director haml
+  end
 end
